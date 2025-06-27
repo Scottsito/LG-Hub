185336c6a7f9817947a8e1d0965fbd2acd5b660e
Rowen <thatcooldudenamedrowen@gmail.com>
2025-06-27 06:09:58 -0400

    Update gxmes.js

--- gxmes.js
+++ gxmes.js
@@ -1,6 +1,5 @@
 const elemsrendered = () => {
-  document.querySelectorAll("h2").forEach(el => el.addEventListener("click",function() {
-    alert("e")
+  document.querySelectorAll("h2").forEach(el => el.addEventListener("click",function()
   location.href = "../play/?" + this.textContent
 }))
 
