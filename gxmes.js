db1ec802bf85112727db01fc78c87f55d08051f4
Rowen <thatcooldudenamedrowen@gmail.com>
2025-06-27 06:16:47 -0400

    Update gxmes.js

--- gxmes.js
+++ gxmes.js
@@ -1,5 +1,6 @@
 const elemsrendered = () => {
-  document.querySelectorAll("h2").forEach(el => el.addEventListener("click",function()
+  document.querySelectorAll("h2").forEach(el => el.addEventListener("click",function() {
+    alert("for some reason it makes you have to press ts just press ok")
   location.href = "../play/?" + this.textContent
 }))
 
